ba442c28564c2ff102f6615fc481b1b235f51de2
Juan Pablo Fernández <juanpf04@ucm.es>
2024-02-03 19:02:35 +0100

    Update Animal.java

--- src/simulator/model/Animal.java
+++ src/simulator/model/Animal.java
@@ -5,6 +5,12 @@ import simulator.misc.Vector2D;
 
 public abstract class Animal implements Entity, AnimalInfo {
 	
+	protected static final double ENERGY = 100.0;
+	protected static final double SPEED = 0.1;
+	protected static final double SCALE = 60.0;
+	protected static final double SIGHT_RANGE = 0.2;
+	protected static final double SPEED2 = 0.2;
+	
 	 protected String _genetic_code;
 	 protected Diet _diet;
 	 protected State _state;
@@ -17,7 +23,7 @@ public abstract class Animal implements Entity, AnimalInfo {
 	 protected double _sight_range;
 	 protected Animal _mate_target;
 	 protected Animal _baby;
-//	 protected AnimalMapView _region_mngr;
+	 protected AnimalMapView _region_mngr;
 	 protected SelectionStrategy _mate_strategy;
 	 
 	 protected Animal(String genetic_code, Diet diet, double sight_range,
@@ -26,17 +32,34 @@ public abstract class Animal implements Entity, AnimalInfo {
 		 this._genetic_code = genetic_code;
 		 this._diet = diet;
 		 this._sight_range = sight_range;
-		 this._speed = Utils.get_randomized_parameter(init_speed, 0.1);
+		 this._speed = Utils.get_randomized_parameter(init_speed, SPEED);
 		 this._mate_strategy = mate_strategy;
 		 this._pos = pos;
 		 
 		 this._state = State.NORMAL;
-		 this._energy = 100.0;
-		 this._desire = 0.0; //hola
+		 this._energy = ENERGY;
+		 this._age = 0.0;			 // revisar
+		 this._desire = 0.0;
 		 this._dest = null;
 		 this._mate_target = null;
 		 this._baby = null;
-//		 this._region_mngr = null;
+		 this._region_mngr = null;
 	 }
 
+	 protected Animal(Animal p1, Animal p2) {
+		 this._dest = null;
+		 this._baby = null;
+		 this._mate_target = null;
+		 this._region_mngr = null;
+		 this._state = State.NORMAL;
+		 this._desire = 0.0;
+		 this._genetic_code = p1._genetic_code;
+		 this._diet = p1._diet;
+		 this._energy = (p1._energy + p2._energy)/2;
+		 this._pos = p1.get_position().plus(Vector2D.get_random_vector(-1,1).scale(SCALE*(Utils._rand
+				 .nextGaussian()+1)));
+		 this._sight_range = Utils.get_randomized_parameter((p1.get_sight_range()+p2.get_sight_range())/2, SIGHT_RANGE);
+		 this._speed = Utils.get_randomized_parameter((p1.get_speed()+p2.get_speed())/2, SPEED2);
+	 }
+	 
 }
