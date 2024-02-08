5cac5869625882d22bc3d7bdad1d070add21dc9a
juanpf04 <juanpf04@ucm.es>
2024-02-08 20:20:18 +0100

    Update RegionManager.java

--- src/simulator/model/RegionManager.java
+++ src/simulator/model/RegionManager.java
@@ -1,6 +1,9 @@
 package simulator.model;
 
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.HashMap;
+import java.util.List;
 
 public class RegionManager implements AnimalMapView {
 	
@@ -25,6 +28,51 @@ public class RegionManager implements AnimalMapView {
 			for (int j = 0; j < this.get_cols(); j++)
 				this._regions[i][j] = new DefaultRegion();
 		
-		// hacer mapa
+		this._animal_region = new HashMap<Animal, Region>();// estructura sin orden
+		// new TreeMap<String, Integer>()// estructura ordenada
 	}
+	
+	
+
+	@Override
+	public int get_cols() {
+		return this._cols;
+	}
+
+	@Override
+	public int get_rows() {
+		return this._rows;
+	}
+
+	@Override
+	public int get_width() {
+		return this._width;
+	}
+
+	@Override
+	public int get_height() {
+		return this._height;
+	}
+
+	@Override
+	public int get_region_width() {
+		return this._region_width;
+	}
+
+	@Override
+	public int get_region_height() {
+		return this._region_height;
+	}
+
+	@Override
+	public double get_food(Animal a, double dt) {
+		//hacer
+		return 0;
+	}
+
+	@Override
+	public List<Animal> get_animals_in_range(Animal e, Predicate<Animal> filter) {
+		return null;
+	}
+	
 }
