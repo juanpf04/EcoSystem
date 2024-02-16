72cf1bdd9f3aea36be9ed0643efc043f7a891795
juanpf04 <juanpf04@ucm.es>
2024-02-16 19:55:23 +0100

    a

--- src/simulator/factories/SheepBuilder.java
+++ src/simulator/factories/SheepBuilder.java
@@ -2,23 +2,24 @@ package simulator.factories;
 
 import org.json.JSONObject;
 
+import simulator.model.SelectFirst;
 import simulator.model.SelectionStrategy;
 import simulator.model.Sheep;
 import simulator.view.Messages;
 
 public class SheepBuilder extends Builder<Sheep> {
 
-	public SheepBuilder() {
+	private Factory<SelectionStrategy> _factory;
+	
+	public SheepBuilder(Factory<SelectionStrategy> factory) {
 		super(Messages.SHEEP_TAG, Messages.DESCRIPTION);
+		this._factory = factory;
 	}
 
 	@Override
-	protected Sheep create_instance(JSONObject data) {
-		
-		
-		SelectionStrategy ms = create_instance(data.getJSONObject("mate_strategy"));
-		SelectionStrategy ds = create_instance(data.getJSONObject("danger_strategy"));		
-	
+	protected Sheep create_instance(JSONObject data) {		
+		SelectionStrategy mate_strategy = new SelectFirst();
+		SelectionStrategy danger_strategy = new SelectFirst();
 		
 		
 		return null;
