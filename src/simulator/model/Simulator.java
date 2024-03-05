9243f1a990185874258418729cce798349875b78
juanpf04 <juanpf04@ucm.es>
2024-03-05 18:38:39 +0100

    observers and notifications

--- src/simulator/model/Simulator.java
+++ src/simulator/model/Simulator.java
@@ -7,16 +7,18 @@ import org.json.JSONObject;
 import simulator.factories.Factory;
 import simulator.view.Messages;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Simulator implements JSONable {
+public class Simulator implements JSONable, Observable<EcoSysObserver> {
 
 	private Factory<Animal> _animals_factory;
 	private Factory<Region> _regions_factory;
 	private RegionManager _region_manager;
 	private List<Animal> _animals;
+	private List<EcoSysObserver> _observers;
 	private double _time;
 
 	public Simulator(int cols, int rows, int width, int height, Factory<Animal> animals_factory,
@@ -35,9 +37,10 @@ public class Simulator implements JSONable {
 
 		this._animals_factory = animals_factory;
 		this._regions_factory = regions_factory;
-		this._region_manager = new RegionManager(cols, rows, width, height);
 		this._animals = new LinkedList<Animal>();
+		this._region_manager = new RegionManager(cols, rows, width, height);
 		this._time = 0.0;
+		this._observers = new LinkedList<EcoSysObserver>();
 	}
 
 	public void reset(int cols, int rows, int width, int height) {
@@ -49,12 +52,14 @@ public class Simulator implements JSONable {
 			throw new IllegalArgumentException(Messages.INVALID_WIDTH);
 		if (height <= 0)
 			throw new IllegalArgumentException(Messages.INVALID_HEIGHT);
-		
+
 		this._animals = new LinkedList<Animal>();
 		this._region_manager = new RegionManager(cols, rows, width, height);
 		this._time = 0.0;
+
+		this.notify_on_reset();
 	}
-	
+
 	private void set_region(int row, int col, Region r) {
 		if (row < 0 || row >= this.get_map_info().get_rows())
 			throw new IllegalArgumentException(Messages.INVALID_ROW);
@@ -64,6 +69,8 @@ public class Simulator implements JSONable {
 			throw new IllegalArgumentException(Messages.INVALID_REGION);
 
 		this._region_manager.set_region(row, col, r);
+
+		this.notify_on_region_set(row, col, r);
 	}
 
 	public void set_region(int row, int col, JSONObject r_json) {
@@ -83,6 +90,8 @@ public class Simulator implements JSONable {
 
 		this._animals.add(a);
 		this._region_manager.register_animal(a);
+
+		this.notify_on_animal_added(a);
 	}
 
 	public void add_animal(JSONObject a_json) {
@@ -113,6 +122,7 @@ public class Simulator implements JSONable {
 		this.update_all_animals(dt);
 		this._region_manager.update_all_regions(dt);
 		this.update_babys();
+		this.notify_on_advance(dt);
 	}
 
 	private void update_babys() {
@@ -168,6 +178,53 @@ public class Simulator implements JSONable {
 		jo.put(Messages.REGIONS_STATE_KEY, this._region_manager.as_JSON());
 
 		return jo;
+	}
+
+	// Observable
+
+	@Override
+	public void addObserver(EcoSysObserver o) {
+		if (!this._observers.contains(o))
+			this._observers.add(o);
+
+		this.notify_on_register(o);
+	}
+
+	@Override
+	public void removeObserver(EcoSysObserver o) {
+		this._observers.remove(o);
+	}
+
+	// Notifications
+	
+	private void notify_on_register(EcoSysObserver o) {
+		o.onRegister(this.get_time(), this.get_map_info(), new ArrayList<>(this.get_animals()));
+	}
+
+	private void notify_on_reset() {
+		List<AnimalInfo> animals = new ArrayList<>(this.get_animals());
+
+		for (EcoSysObserver o : this._observers)
+			o.onReset(this.get_time(), this.get_map_info(), animals);
+	}
+
+	private void notify_on_animal_added(Animal a) {
+		List<AnimalInfo> animals = new ArrayList<>(this.get_animals());
+
+		for (EcoSysObserver o : this._observers)
+			o.onAnimalAdded(this.get_time(), this.get_map_info(), animals, a);
+	}
+
+	private void notify_on_region_set(int row, int col, Region r) {
+		for (EcoSysObserver o : this._observers)
+			o.onRegionSet(row, col, this.get_map_info(), r);
+	}
+
+	private void notify_on_advance(double dt) {
+		List<AnimalInfo> animals = new ArrayList<>(this.get_animals());
+
+		for (EcoSysObserver o : this._observers)
+			o.onAvanced(this.get_time(), this.get_map_info(), animals, dt);
 	}
 
 }
