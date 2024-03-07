f943eda9292b5b6cc80872bf9bb259a27dff493b
dcastr06 <dcastr06@ucm.es>
2024-03-07 09:32:23 +0100

    view

--- src/simulator/view/AbstractMapViewer.java
+++ src/simulator/view/AbstractMapViewer.java
@@ -1,5 +1,16 @@
 package simulator.view;
 
-public class AbstractMapViewer {
+import java.util.List;
 
+import javax.swing.JComponent;
+
+import simulator.model.AnimalInfo;
+import simulator.model.MapInfo;
+
+@SuppressWarnings("serial")
+public abstract class AbstractMapViewer extends JComponent {
+
+	public abstract void update(List<AnimalInfo> objs, Double time);
+
+	public abstract void reset(double time, MapInfo map, List<AnimalInfo> animals);
 }
