ca6ba0e97a89f1301da4758a63ef798ca2d65e5c
dcastr06 <dcastr06@ucm.es>
2024-03-07 20:57:00 +0100

    ctrPanel-StatusBar

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -10,6 +10,8 @@ import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
 
 import simulator.control.Controller;
 import simulator.launcher.Main;
@@ -50,10 +52,10 @@ public class ControlPanel extends JPanel {
 		this._toolaBar = new JToolBar();
 		this.add(_toolaBar, BorderLayout.PAGE_START);
 
-		// TODO crear los diferentes botones/atributos y a�adirlos a _toolaBar.
+		/*TODO crear los diferentes botones/atributos y añadirlos a _toolaBar.
 		// Todos ellos han de tener su correspondiente tooltip. Puedes utilizar
 		// _toolaBar.addSeparator() para a�adir la l�nea de separaci�n vertical
-		// entre las componentes que lo necesiten.
+		// entre las componentes que lo necesiten.*/
 
 		// Open Button
 		this._openButton = new JButton();
@@ -67,7 +69,7 @@ public class ControlPanel extends JPanel {
 		this._viewerButton = new JButton();
 		this._viewerButton.setToolTipText("Viewer");
 		this._viewerButton.setIcon(new ImageIcon("resources/icons/viewer.png"));
-		this._viewerButton.addActionListener((e) -> ); // TODO check
+		this._viewerButton.addActionListener((e) -> {MapWindow mapWindow = new MapWindow();}); // TODO check
 		this._toolaBar.add(this._viewerButton);
 
 		// Regions Button
@@ -82,12 +84,15 @@ public class ControlPanel extends JPanel {
 		this._runButton.setToolTipText("Run");
 		this._runButton.setIcon(new ImageIcon("resources/icons/run.png"));
 		this._runButton.addActionListener((e) -> {
-			this._openButton.setVisible(false);
-			this._viewerButton.setVisible(false);
-			this._regionsButton.setVisible(false);
-			this._quitButton.setVisible(false);
+			this._openButton.setEnabled(false);
+			this._viewerButton.setEnabled(false);
+			this._regionsButton.setEnabled(false);
+			this._quitButton.setEnabled(false);
 			this._stopped = false;
 			
+			this._delta_time =  this._textField.getText();
+			
+			this.run_sim((Integer) this._spinner.getValue(), this._delta_time); //TODO REVISAR
 		}); // TODO check
 		this._toolaBar.add(this._runButton);
 
@@ -96,10 +101,19 @@ public class ControlPanel extends JPanel {
 		this._stopButton = new JButton();
 		this._stopButton.setToolTipText("Stop");
 		this._stopButton.setIcon(new ImageIcon("resources/icons/stop.png"));
-		this._stopButton.addActionListener((e) -> this._fc.showOpenDialog(ViewUtils.getWindow(this))); // TODO check
+		this._stopButton.addActionListener((e) -> this._stopped = true); // TODO check
 		this._toolaBar.add(this._stopButton);
 
 		//spinner y lo otro
+		//Spinner
+		
+		_spinner = new JSpinner(new SpinnerNumberModel(5000, 1, 10000, 100));
+		this._toolaBar.add(this._spinner);
+		
+		//JTextField
+		this._textField= new JTextField(10);
+		this._textField.setBounds(100,50,120,30); // ¿Donde colocar el txtField?
+		this._toolaBar.add(this._textField);
 		
 		// Quit Button
 		this._toolaBar.add(Box.createGlue()); // this aligns the button to the right
@@ -119,5 +133,36 @@ public class ControlPanel extends JPanel {
 		// de regiones
 	}
 	// TODO el resto de m�todos van aqu�
+	
+	private void run_sim(int n, double dt) {
+		if (n > 0 && !_stopped) {
+			try {
+				_ctrl.advance(dt);
+				SwingUtilities.invokeLater(() -> run_sim(n - 1, dt));
+			} catch (Exception e) {
+				// TODO llamar a ViewUtils.showErrorMsg con el mensaje de error
+				// que corresponda
+				
+				ViewUtils.showErrorMsg("Mensaje de error"); // cual es
 
+				// TODO activar todos los botones
+				
+				this.enableButtons();
+
+				_stopped = true;
+			}
+		} else {
+			// TODO activar todos los botones
+			this.enableButtons();
+			_stopped = true;
+		}
+	}
+	private void enableButtons() {
+		this._openButton.setEnabled(true);
+		this._viewerButton.setEnabled(true);
+		this._regionsButton.setEnabled(true);
+		this._runButton.setEnabled(true);
+		this._stopButton.setEnabled(true);
+		this._quitButton.setEnabled(true);
+	}
 }
