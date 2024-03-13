fb25909b48c244745c03f8793ccce213a0df9d6a
dcastr06 <dcastr06@ucm.es>
2024-03-13 11:52:26 +0100

    CreateTable

--- src/simulator/view/InfoTable.java
+++ src/simulator/view/InfoTable.java
@@ -2,6 +2,8 @@ package simulator.view;
 
 import javax.swing.JPanel;
 import javax.swing.table.TableModel;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
 
 public class InfoTable extends JPanel {
 	String _title;
@@ -18,5 +20,9 @@ public class InfoTable extends JPanel {
 	 // TODO añadir un borde con título al JPanel, con el texto _title
 	 // TODO añadir un JTable (con barra de desplazamiento vertical) que use
 	 //_tableModel
+		
+	JTable table = new JTable(this._tableModel);
+	JScrollPane scroll = new
+			JScrollPane (table , JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 	 }
 }
