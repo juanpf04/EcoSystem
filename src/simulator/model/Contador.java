5c59d14cfcf97e2168ddf362a1339245dba45716
juanpf04 <juanpf04@ucm.es>
2024-05-14 21:16:18 +0200

    mejoras

--- src/simulator/model/Contador.java
+++ src/simulator/model/Contador.java
@@ -1,9 +1,10 @@
 package simulator.model;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import simulator.control.Controller;
 import simulator.model.Animal.Diet;
@@ -11,15 +12,29 @@ import simulator.model.MapInfo.RegionData;
 
 public class Contador implements EcoSysObserver {
 
-	private Map<RegionData, Integer> contador;
+	private Map<RegionData, Integer> _contador;
 
 	public Contador(Controller ctrl) {
-		this.contador = new HashMap<MapInfo.RegionData, Integer>();
+		this._contador = new TreeMap<>(new Comparator<RegionData>() {
+
+			@Override
+			public int compare(RegionData r1, RegionData r2) {
+				if (r1.row() == r2.row() && r1.col() == r2.col())
+					return 0;
+				if (r1.row() < r2.row() || (r1.row() == r2.row() && r1.col() < r2.col()))
+					return -1;
+
+				return 1;
+			}
+		});
 		ctrl.addObserver(this);
 	}
 
 	@Override
 	public void onRegister(double time, MapInfo map, List<AnimalInfo> animals) {
+		for (RegionData r : map) {
+			this._contador.put(r, 0);
+		}
 	}
 
 	@Override
@@ -36,29 +51,29 @@ public class Contador implements EcoSysObserver {
 
 	@Override
 	public void onAvanced(double time, MapInfo map, List<AnimalInfo> animals, double dt) {
+		// recorrer las regiones
+		// para cada region comprobar si hay 3 carnivoros en ella
+		// si los hay aumento su contador, si no no
 
 		for (RegionData r : map) {
-			Integer cont = this.contador.get(r);
-			if(cont == null) {
-				if(r.region().getAnimalsInfo().stream().filter((a)->a.get_diet() == Diet.CARNIVORE).count() >= 3)
-					cont = 1;
-				else 
-					cont = 0;
+			Integer contador = this._contador.get(r);
+
+			long numCarnivoros = r.region().getAnimalsInfo().stream().filter((a) -> a.get_diet() == Diet.CARNIVORE)
+					.count();
+
+			if (numCarnivoros >= 3) {
+				contador++;
+				this._contador.replace(r, contador);
 			}
-			else {
-				if(r.region().getAnimalsInfo().stream().filter((a)->a.get_diet() == Diet.CARNIVORE).count() >= 3)
-					cont++;
-			}
-			this.contador.put(r, cont);
 		}
 	}
 
 	public void imprimeInfo() {
+		// region y su contador
 		System.out.println("numero de veces que hay 3 carnivoros en cada region");
-		for(Entry<RegionData, Integer> pair :contador.entrySet()) {
+		for (Entry<RegionData, Integer> pair : this._contador.entrySet()) {
 			RegionData r = pair.getKey();
-			System.out.println("Region ("+r.row() + "," + r.col() + "): " + pair.getValue());
+			System.out.println(r.region() + " region (" + r.row() + "," + r.col() + "): " + pair.getValue());
 		}
 	}
-
 }
